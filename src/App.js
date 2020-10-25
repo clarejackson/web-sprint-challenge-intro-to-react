1409f0bcb89066ec5e58b194f984dd838415b3ca
Clare Jackson <clare-jackson@lambdastudents.com>
2020-10-25 19:36:53 -0400

    All character cards rendering.

--- src/App.js
+++ src/App.js
@@ -1,17 +1,32 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
+import axios from 'axios';
+import CharacterCard from './components/Character'
 
 const App = () => {
+  const [character, setCharacter] = useState([])
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
-
+  useEffect(() => {
+    axios
+      .get(`https://rickandmortyapi.com/api/character`)
+      .then(response => {
+        console.log(response.data.results);
+        setCharacter(response.data.results)
+    })
+    .catch(error => {
+      console.log('the data was not returned', error)
+    })
+  }, [])
   // Fetch characters from the API in an effect hook. Remember, anytime you have a 
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
 
   return (
     <div className="App">
-      <h1 className="Header">Characters</h1>
+      {character.map( (item) => 
+      <CharacterCard character={item} className="Header" />
+      )}
     </div>
   );
 }
--- src/components/Character.js
+++ src/components/Character.js
@@ -1,1 +1,17 @@
 // Write your Character component here
+
+import React from 'react';
+// import 
+
+const CharacterCard = (props) => {
+  const { name, image } = props.character
+   
+  return (
+    <div>
+      <h1>{name}</h1>
+      <img src={image} />
+    </div>
+  )
+}
+
+export default CharacterCard;
